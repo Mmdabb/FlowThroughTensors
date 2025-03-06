62adc6392466418f4127dfcd1c51bb9facfe29ab
Mohammad <92270125+Mmdabb@users.noreply.github.com>
2025-03-06 12:05:40 -0700

    Create FlowThroughTensors.h

---
+++ src/FlowThroughTensors.h
@@ -0,0 +1,12 @@
+#ifndef GUARD_PATH_ENGINE_H
+#define GUARD_PATH_ENGINE_H
+
+#ifdef _WIN32
+#define PATH_ENGINE_API __declspec(dllexport)
+#else
+#define PATH_ENGINE_API
+#endif
+
+extern "C" PATH_ENGINE_API void DTA_AssignmentAPI();
+
+extern "C" PATH_ENGINE_API void DTA_SimulationAPI();
